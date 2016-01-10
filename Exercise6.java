526bc950410f810a8f2d9af7675e7b05e8036ed5
Alina_Ov <Alina_Ov>
2016-01-10 11:11:55 +0200

    Exercise6 new

---
+++ Exercise6.java
@@ -0,0 +1,18 @@
+// Упражнение 6
+class Exercise6{
+public static void main(String[] args){
+	
+	double TC;
+	double TF;
+	
+	TF = 189;
+	System.out.println("The Fahrenheit temp is " + TF);
+	
+	TC = (TF - 32)*5/9;
+	
+	System.out.println("The Celsius temp is " + TC);
+	
+}
+
+
+}
