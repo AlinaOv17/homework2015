713833448b0826beedea89f17dcac8dbb5f602c8
Alina_Ov <Alina_Ov>
2016-01-10 11:10:15 +0200

    Exercise5 new

---
+++ Exercise5.java
@@ -0,0 +1,27 @@
+// Упражнение 5
+class Exercise5{
+public static void main(String[] args){
+	
+	int A = 3;
+	int B;
+
+    System.out.println("The val of var A is " + A);
+	
+	
+	B = A*A;
+	
+	System.out.println("Squared 3 is " + B);
+	
+	B = B*A;
+	System.out.println("3 to the power of 3 is " + B);
+	
+	B = B*A;
+	System.out.println("3 to the power of 4 is " +B);
+	
+	B = B*B;
+	System.out.println("3 to the power of 8 is " + B);
+	
+	
+}
+
+}
