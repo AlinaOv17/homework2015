22a4e1be1a36169fb5d6e23ae108bff26af6dd7a
Alina_Ov <Alina_Ov>
2016-01-10 11:13:36 +0200

    Exercise7 new

---
+++ Exercise007.java
@@ -0,0 +1,16 @@
+// Упражнение 07
+class Exercise007{
+public static void main(String[] args){
+	int C = 5;
+	
+System.out.println("The val of temp in Celsius is " + C);
+
+int F = (C * 9 / 5) +32;
+
+System.out.println("The val of temp in Fahrenheit is " + F);
+
+
+}
+
+
+}
