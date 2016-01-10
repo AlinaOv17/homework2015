542b7bd93ce7150cc8a1e0a9a07c7ed5bc10adc2
Alina_Ov <Alina_Ov>
2016-01-10 11:15:35 +0200

    Exercise16 new

---
+++ Exercise16.java
@@ -0,0 +1,12 @@
+class Exercise16{
+public static void main(String[] args){
+
+
+int K = 245;
+System.out.println("The number of a day in a year " + K);
+int A = ( 3 + K - 1) % 7;
+System.out.println("The number of the day in the week is " + A); 
+
+}
+}
+
