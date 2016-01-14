cf85c8e6fa9dd162e8f1921aa06821915746c873
Alina_Ov <Alina_Ov>
2016-01-15 01:39:24 +0200

    Erased the third line ( and understood why we dont need it)

--- Task1.java
+++ Task1.java
@@ -1,6 +1,6 @@
 class Task1{
 public static void main(String[] args) {
-          String name = args[0];
+         
             for (int i = 0; i < args.length; i++)
                 System.out.println("Hello " + args[i] +"!!!");
         }
