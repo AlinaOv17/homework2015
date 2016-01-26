e2c3964ef352b17dceb69f1eea30c8730a8d2b73
Alina_Ov <Alina_Ov>
2016-01-26 14:24:49 +0200

    Exercise 57 without array used + some changes

--- Exercise057.java
+++ Exercise057.java
@@ -2,37 +2,42 @@ import java.util.Scanner;
  
 public class Exercise057 {
     public static void main(String[] args) {
- 
-        int[] a = new int[100];
-        int aLength = a.length;
-        int rand = (int) (Math.random() * aLength);
-        int x = rand;
-		int tries = 0;
+        int x = (int) (Math.random() * 100) + 1; // random number
+        int tries = 0;
         int guess;
 		Scanner sc = new Scanner(System.in);
-		
 		boolean win = false;
-		  System.out.println("Guessed numb " + x);
+		System.out.println("Guessed numb " + x);
 		
 		while (win == false) {
-         
             System.out.println( "Type the number : " );
             guess=sc.nextInt();
-             
             tries++;
              
-            if (guess == x) {
+        if (guess == x) {
                 win = true;
             } 
-            else if (guess < x) {
+        else if (guess < x) {
                 System.out.println("Try again - guessed number is bigger!");
             }
-            else if (guess > x) {
+        else if (guess > x) {
                 System.out.println("Try again - guessed number is smaller!");
             }
         }
-        System.out.println("You guessed the number " + x);
-		System.out.println("You tried " + tries);
+		if (tries < 3){
+			
+			System.out.println("You guessed a number, but looks like you're a cheater!");
+			System.out.println("You tried " + tries);
+		}
+		else if(tries >= 3 && tries < 7){
+		System.out.println(	"Congrats, you guessed a number " + x);
+	    System.out.println("You tried " + tries);
+		}
+        else if (tries >=7){
+			System.out.println("You guessed a number, but can be better!");
+            System.out.println("You tried " + tries);
+			
+		}
       
     }
 }
