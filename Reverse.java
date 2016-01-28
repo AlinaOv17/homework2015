7d4b0321614252701e22e7d2195dfecdca1e4945
Alina_Ov <Alina_Ov>
2016-01-29 01:05:53 +0200

    Reverse

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+class Reverse{
+public static void main(String[] args){
+int[] array = {2, 3, 4, 5, 6, 7, 8, 9};
+for (int i = 0; i <  array.length; ++i){
+	System.out.print (array[i]);
+	System.out.print(" ");
+}
+    System.out.println(" ");
+
+for (int i = (array.length-1); i >= 0; i--){
+	System.out.print (array[i]);
+	System.out.print(" ");
+	
+}
+
+}
+}
