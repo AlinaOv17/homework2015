c2d0632845f23d6900f752de019bac7aa98f5897
Alina_Ov <Alina_Ov>
2016-02-01 21:37:02 +0200

    This does not work Exercise47

---
+++ Exercise47.java
@@ -0,0 +1,21 @@
+class Exercise47{
+	public static void main(String[] args){
+	String text = "This is the text where to find a character";
+	System.out.println (text);
+       
+	countChars( "This is the text where to find a character", 't'); 
+	
+		    
+	static int countChars(String textt, char search){
+	int times = 0;
+	char ch;
+	for (int i =0; i < textt.length(); i++){
+		ch = textt.charAt(i);
+		if (search == ch){
+		times++;}
+		}
+	return times;
+	System.out.println(times);
+}
+}
+}
