090acebb956335d79df053fe012bd997a2e42f7f
Alina_Ov <Alina_Ov>
2016-02-02 17:06:40 +0200

    to make the last loop

---
+++ Rightcln1.java
@@ -0,0 +1,38 @@
+class Rightcln1{
+public static void main(String[] args){
+int[] array = {2, 3, 4, 5, 6, 7, 8, 9};
+int n = 3;
+int i;
+int temp =0;
+// Sdvig Vpravo
+for (i = 0; i <  array.length; ++i){
+	System.out.print (array[i]);
+	System.out.print(" ");
+}
+	System.out.println(" ");
+	
+        
+     for (i = (array.length-1-n); i >= 0; i--){
+	System.out.print (array[i]);
+	System.out.print(" ");
+	
+		}
+		
+	for (i = (array.length-1); i > (array.length-1-n); i--){
+	System.out.print (array[i]);
+	System.out.print(" ");
+		}
+		
+		System.out.println(" ");
+		
+	
+	
+	
+	System.out.print (array[i]);
+	System.out.print(" ");
+	
+	
+}		
+	
+}		
+}
